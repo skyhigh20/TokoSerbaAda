986ddd1a3efe684f09b617edd9206715755a85fc
skyhigh20 <nadhifryanalvaro@gmail.com>
2024-09-09 15:16:08 +0700

    Penambahan status

---
+++ TokoSerbaGuna/settings.py
@@ -0,0 +1,17 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'main',  
+]
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
