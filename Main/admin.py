801124a801e5ee8810b8f61c747ae37b5a5b4275
skyhigh20 <nadhifryanalvaro@gmail.com>
2024-09-10 16:42:42 +0700

    Penambahan file lagi

--- Main/admin.py
+++ Main/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'description']  # Menampilkan kolom ini di situs admin
--- Main/models.py
+++ Main/models.py
@@ -4,8 +4,6 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
-    stock = models.IntegerField(default=0)
-    category = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return self.name
